1b0141adc7e29ab899692df64b93d73562d7f8d5
Koooooooooooooo1 <160296120+Koooooooooooooo1@users.noreply.github.com>
2024-04-18 11:22:07 +0800

    H

---
+++ H/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "加拿大渥太華 漂漂圖書館",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -0.9589218483040831,
+        "pitch": -0.05624677980207515,
+        "fov": 1.3419674740723089
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
