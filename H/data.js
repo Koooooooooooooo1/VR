0e78c53b5d783462fe8c16be71037ef78f244aef
Koooooooooooooo1 <160296120+Koooooooooooooo1@users.noreply.github.com>
2024-05-02 11:40:27 +0800

    H

---
+++ H/data.js
@@ -0,0 +1,61 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "加拿大渥太華 國會圖書館",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.08322505627976,
+        "pitch": 0,
+        "fov": 1.3419674740723089
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 3.021450223407795,
+          "pitch": -0.3566100165802535,
+          "title": "<b>館藏數量</b>",
+          "text": "<div>此館蒐集側重於加拿大之政治、法律及立法、國會事務與議事程序等國會相關資料，雖非法定送存機構，其國內發行之出版品無須呈繳該館，但書架總長度逾 17 公里之館藏資源，涵括圖書、期刊、政府文獻、CD-ROM 等，主要典藏資料為：</div><div><br></div><div>（一）圖書：約 60 萬冊</div><div>（二）期刊：約 8,900 種<br></div><div><br></div><div><br></div>"
+        },
+        {
+          "yaw": -1.1672172608631524,
+          "pitch": 0.0405908865172524,
+          "title": "<b>服務對象</b>",
+          "text": "<div>該館旨在提供立法研究及參考諮詢服務，俾協助國會議員遂行職務，</div><div>而非提供一般服務之公共圖書館，不具國家圖書館之功能，故未開放一般民眾使用，其服務對象為：</div><div><br></div><div>（一）國會</div><div>（二）國會議員及其幕僚</div><div>（三）國會委員會及協會</div><div>（四）協助國會之組織</div><div>（五）國會議員授權之一般民眾</div><div><br></div><div><br></div>"
+        },
+        {
+          "yaw": -1.9322086529177671,
+          "pitch": 0.2801639291526872,
+          "title": "<b>服務項目</b>",
+          "text": "<div>1. 為國會議員及其幕僚提供特定議題之研究與分析報告。<br></div><div>2. 為國會議員通告、更新及傳遞所需之新聞。</div><div>3. 提供立法者及委員會有關審議事項之資訊。</div><div>4. 保存國會文獻資產，並確保館藏之利用。</div><div>5. 協助國會議員為加拿大民眾提供國會及議案審議前之相關資訊。</div><div><br></div><div><br></div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
