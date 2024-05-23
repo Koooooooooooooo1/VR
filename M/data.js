5a8c1ef0260f8121b39060cbb3593e63eadd92a0
Koooooooooooooo1 <160296120+Koooooooooooooo1@users.noreply.github.com>
2024-05-23 11:46:15 +0800

    1

---
+++ M/data.js
@@ -0,0 +1,723 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "千禧橋",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.03319668688132538,
+        "pitch": -0.21025373922394586,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1022968711272476,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.39613633341101995,
+          "pitch": -0.4301422084288351,
+          "title": "<div><b>千禧橋</b></div>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.7341556843145334,
+          "pitch": -0.02169082810989309,
+          "title": "<b>泰晤士河</b>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.1500056766413902,
+          "pitch": -0.22738871234386515,
+          "title": "<b>倫敦市</b>",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.9282087123760974,
+          "pitch": -0.4104160993341601,
+          "title": "<b>班克賽德</b>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "利德賀市場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -2.036681921310363,
+        "pitch": 0,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9860658888036973,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -1.4859860706628947,
+          "pitch": -0.0017673797114525769,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7477871412226058,
+          "pitch": -0.06537793896382205,
+          "title": "<b>維多利亞式建築</b>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "聖潘克拉斯車站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.3693465147805863,
+        "pitch": -0.37044706434697083,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7150991740607431,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 1.5804065588921858,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.175676403660482,
+          "pitch": -0.17515152477456297,
+          "title": "<b>哥德復興式建築</b>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "國王十字車站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.4382219131060303,
+        "pitch": -0.2578111326198318,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8550188575411752,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.6325906787885671,
+          "pitch": 0.005254542421534225,
+          "rotation": 0,
+          "target": "4-9"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.07562020456675711,
+          "pitch": -0.2364284033902031,
+          "title": "<b>維多利亞式建築</b>",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.2009982041317926,
+          "pitch": -0.11952728183890571,
+          "title": "<b>康登鎮</b>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.6890995293851834,
+          "pitch": -0.23113084349591517,
+          "title": "<b>伊斯靈頓</b>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-9",
+      "name": "哈利波特9¾月台商店",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 1.5880073341033771,
+        "pitch": -0.07759364185646334,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.082870282542933,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.12639249501974,
+          "pitch": -0.017234460172341315,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.7775188178857206,
+          "pitch": 0.18843471050042915,
+          "title": "<b>9¾月台</b>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "牛津大學基督堂學院",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.010685689297925904,
+        "pitch": -0.08009666256150894,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0838350183621035,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "4-9"
+        },
+        {
+          "yaw": -1.599227673966226,
+          "pitch": -0.005863623335438106,
+          "rotation": 0,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.08130841346189044,
+          "pitch": -0.3301537617983765,
+          "title": "<b>湯姆塔</b>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.1058408002729543,
+          "pitch": -0.16224441837629655,
+          "title": "<b>湯姆四合院</b>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-",
+      "name": "牛津大學基督堂學院餐廳",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1408,
+      "initialViewParameters": {
+        "yaw": -3.1071736175070566,
+        "pitch": -0.15268437341077146,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4440486487958157,
+          "pitch": -0.002936382673441429,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 2.175306409892646,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "博德利圖書館",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.19296704216251825,
+        "pitch": -0.28033831896527417,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3954684352290094,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 0.4168936856817336,
+          "pitch": -0.0021486396690981735,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.11833341791255769,
+          "pitch": -0.41460551054301575,
+          "title": "<b>英國哥德式建築</b>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "8-",
+      "name": "牛津大學新學院",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.2631084826654604,
+        "pitch": -0.2152597806340406,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9702387034232558,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": 0.08201881865460692,
+          "pitch": 0.008818267330036988,
+          "rotation": 0,
+          "target": "9-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-",
+      "name": "馬勒姆灣",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.0841143706499246,
+        "pitch": 0.04766145745306005,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2379666677432333,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "8-"
+        },
+        {
+          "yaw": 0.5080181790222973,
+          "pitch": 0.0018849532601432628,
+          "rotation": 0,
+          "target": "10-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-",
+      "name": "達勒姆座堂",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.4944377543711109,
+        "pitch": -0.549371665467568,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8348923088418161,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "11-"
+        },
+        {
+          "yaw": 2.7333006544829557,
+          "pitch": 0.00478379951029595,
+          "rotation": 0,
+          "target": "9-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.08870234024713142,
+          "pitch": -0.27376853869033013,
+          "title": "<b>羅馬式拱頂建築</b>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "11-",
+      "name": "阿尼克城堡",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.08013152431076875,
+        "pitch": -0.24290843669163031,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5988774025705101,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "10-"
+        },
+        {
+          "yaw": 0.22833157677606053,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "12-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-",
+      "name": "埃蒂夫湖",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1812.5,
+      "initialViewParameters": {
+        "yaw": 1.2058388733937377,
+        "pitch": 0.10531622604322877,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.332551142404381,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "11-"
+        },
+        {
+          "yaw": -1.2853979957482036,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "13-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-",
+      "name": "希爾湖",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.5695693920947686,
+        "pitch": 0,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10532855174488809,
+          "pitch": 0.0028689854331638287,
+          "rotation": 0,
+          "target": "14-"
+        },
+        {
+          "yaw": 1.3662757742173408,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "12-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-",
+      "name": "格蘭芬蘭高架橋",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.2671833179428411,
+        "pitch": 0.03388852146711052,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0927823678813215,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "13-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
---
+++ USJ/data.js
@@ -0,0 +1,282 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "北京環球影城",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.3216432859853064,
+        "pitch": -0.16019332512301432,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4797619065697614,
+          "pitch": 0.007665516439304909,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "日本環球影城",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1408,
+      "initialViewParameters": {
+        "yaw": 1.1957982516008627,
+        "pitch": -0.1727300392894371,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4794164575554767,
+          "pitch": -0.06075829631269514,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -2.8366847842369367,
+          "pitch": -0.04579567447882127,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "日本霍格華茲城",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.07877080015646243,
+        "pitch": -0.03253926916561234,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5681798745928273,
+          "pitch": -0.020616568806786972,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.379683768418973,
+          "pitch": -0.02222002910293952,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "好萊塢環球影城",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.023151644275781535,
+        "pitch": -0.18522353217347032,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3140611961817985,
+          "pitch": -0.06654152174784045,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.0495001027295316,
+          "pitch": -0.013838193665343113,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "好萊塢霍格華茲城",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.20302809666056376,
+        "pitch": -0.1727079904016815,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.477560608324765,
+          "pitch": 0.04241077919345315,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 1.9323281914256958,
+          "pitch": 0.02096206075692919,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "奧蘭多環球影城",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1792,
+      "initialViewParameters": {
+        "yaw": -0.008904741081590117,
+        "pitch": -0.48808903748414956,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-",
+      "name": "奧蘭多霍格華茲城",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": -0.08010370418660528,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3694390776419034,
+          "pitch": 0.02285131760493897,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
